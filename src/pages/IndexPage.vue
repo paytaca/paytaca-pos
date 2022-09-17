d52bd3b872d2523925674ad7598673c92dcc31c8
joemarct <joemar.ct@gmail.com>
2022-09-17 15:26:08 +0800

    Initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Paytaca logo"
+      src="~assets/paytaca_logo.png"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
