02cd7190d1f2c8dc3949c973f98254ac8ccf7a92
khirvy019 <khirvy_19@yahoo.com>
2025-08-18 10:02:39 +0800

    remove unused code for market prices storage

--- src/stores/market.js
+++ src/stores/market.js
@@ -3,9 +3,6 @@ import { defineStore } from 'pinia';
 
 export const useMarketStore = defineStore('market', {
   state: () => ({
-    usdRates: [
-      { currency: 'USD', rate: 1, timestamp: 0 },
-    ],
     bchRates: [
       { currency: '', rate: 0, timestamp: 0 },
     ],
@@ -105,85 +102,5 @@ export const useMarketStore = defineStore('market', {
           return newRate
         })
     },
-    /**
-     * @param {String} currency 
-     * @param {Object} opts 
-     * @param {Number} opts.age will skip update when rate timestamp is less than age of 
-     */
-    refreshBchPriceOld(currency, opts) {
-      if (!currency) return Promise.reject()
-      if (!Array.isArray(this.bchRates)) return Promise.reject()
-
-      currency = currency.toUpperCase()
-
-      const rate = this.bchRates.find(_rate => _rate?.currency === currency)
-      if (rate?.timestamp && opts?.age && Date.now() - opts?.age <= rate?.timestamp) {
-        return Promise.resolve(rate)
-      }
-
-      const getUsdRates = []
-      const vsCurrencies = []
-      if (currency === 'ARS') {
-        getUsdRates.push(currency)
-        vsCurrencies.push('USD')
-      } else {
-        vsCurrencies.push(currency)
-      }
-
-      getUsdRates.map(this.refreshUsdPrice)
-      let url = 'https://api.coingecko.com/api/v3/simple/price/'
-      const headers = {}
-      const params = { ids: 'bitcoin-cash', vs_currencies: vsCurrencies.join(',') }
-      if (process.env.COINGECKO_API_KEY) {
-        url = 'https://pro-api.coingecko.com/api/v3/simple/price/'
-        headers['x-cg-pro-api-key'] = process.env.COINGECKO_API_KEY
-      }
-      return axios.get(url, { headers, params })
-        .then(response => {
-          const bchRatesData = response?.data?.['bitcoin-cash']
-          if (!bchRatesData) return Promise.reject({ response })
-
-          const newRates = Object.getOwnPropertyNames(bchRatesData).forEach(_currency => {
-           const rateValue = bchRatesData[_currency]
-           if (!rateValue) return
-           const newRate = { currency: _currency.toLocaleUpperCase(), rate: rateValue, timestamp: Date.now() }
-            this.updateBchPrice(newRate)
-          }) 
-          return Promise.resolve(newRates)
-        })
-    },
-    /**
-     * 
-     * @param {Object} newRate 
-     * @param {String} newRate.currency
-     * @param {Number} newRate.rate
-     * @param {Number} newRate.timestamp
-     */
-    updateUsdPrice(newRate) {
-      const index = this.usdRates.findIndex(_rate => _rate?.currency === newRate.currency)
-      if (index >= 0) this.usdRates[index] = newRate
-      else this.usdRates.push(newRate)
-    },
-    /**
-     * @param {String} currency 
-     * @param {Object} opts 
-     * @param {Number} opts.age will skip update when rate timestamp is less than age of 
-     */
-    async refreshUsdPrice(currency, opts) {
-      if (!currency) return Promise.reject()
-      const _currency = currency.toUpperCase()
-
-      const rate = this.usdRates.find(rateData => rateData.currency === _currency)
-      if (rate?.timestamp && opts?.age && Date.now() - opts?.age <= rate?.timestamp) {
-        return Promise.resolve(rate)
-      }
-
-      const { data } = await axios.get(`https://api.yadio.io/rate/${_currency}/USD`)
-      if (!data.rate) return Promise.reject()
-      const newRate = { currency: String(_currency), rate: data.rate, timestamp: data?.timestamp }
-      this.updateUsdPrice(newRate)
-
-      return newRate
-    }
   },
 })
