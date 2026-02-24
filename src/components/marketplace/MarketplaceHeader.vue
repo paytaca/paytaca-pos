6d66c08c9fe66d4f5f5baf9ee1457a911fbb7832
joemarct <joemar.ct@gmail.com>
2026-02-24 22:08:44 +0800

    Fix iOS safe area padding on marketplace auth pages
    - Remove inline iOS padding from MarketplaceHeader and LoginPage
    - Add CSS rule for pages with q-pa-* padding classes to combine safe area padding with existing padding
    - Ensure proper top margin on marketplace login and register pages

--- src/components/marketplace/MarketplaceHeader.vue
+++ src/components/marketplace/MarketplaceHeader.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="row no-wrap q-mt-sm q-mb-md q-r-ml-lg" :style="$q.platform.is.ios ? 'padding-top: 35px;' : ''">
+  <div class="row no-wrap q-mt-sm q-mb-md q-r-ml-lg">
     <slot name="title">
       <div class="q-space">
         <div class="text-h4">{{ title }}</div>
       </div>
     </slot>
     <div>
-      <div class="user-icon" role="button" @click="() => openUserMenu = true">
-        <q-img :src="userImage" style="width:100%;border-radius: 999px;" ratio="1"/>
+      <div class="user-icon" role="button" @click="() => (openUserMenu = true)">
+        <q-img
+          :src="userImage"
+          style="width: 100%; border-radius: 999px"
+          ratio="1"
+        />
       </div>
     </div>
     <q-dialog v-model="openUserMenu" position="bottom">
@@ -15,7 +19,8 @@
         <q-card-section>
           <div class="row items-start">
             <img
-              :src="userImage" style="width:50px"
+              :src="userImage"
+              style="width: 50px"
               class="rounded-borders q-mr-sm self-center"
               @click="$router.push({ name: 'marketplace-user' })"
             />
@@ -29,7 +34,7 @@
                 {{ marketplaceStore.user.username }}
               </div>
             </div>
-            <q-space/>
+            <q-space />
             <div>
               <q-btn
                 outline
@@ -42,22 +47,33 @@
               />
             </div>
           </div>
-          <div v-if="marketplaceStore?.userRoles?.length" class="q-mt-xs q-gutter-sm">
-            <q-badge v-for="(role, index) in marketplaceStore?.userRoles" :key="index">
+          <div
+            v-if="marketplaceStore?.userRoles?.length"
+            class="q-mt-xs q-gutter-sm"
+          >
+            <q-badge
+              v-for="(role, index) in marketplaceStore?.userRoles"
+              :key="index"
+            >
               {{ formatRole(role) }}
             </q-badge>
           </div>
         </q-card-section>
         <q-list>
-          <q-item clickable v-ripple v-close-popup :to="{ name: 'marketplace-user' }">
+          <q-item
+            clickable
+            v-ripple
+            v-close-popup
+            :to="{ name: 'marketplace-user' }"
+          >
             <q-item-section>
-              <q-item-label>{{ $t('ChangePassword') }}</q-item-label>
+              <q-item-label>{{ $t("ChangePassword") }}</q-item-label>
             </q-item-section>
           </q-item>
-          <q-separator/>
+          <q-separator />
           <q-item clickable v-ripple v-close-popup @click="() => logOut()">
             <q-item-section>
-              <q-item-label>{{ $t('Logout') }}</q-item-label>
+              <q-item-label>{{ $t("Logout") }}</q-item-label>
             </q-item-section>
           </q-item>
         </q-list>
@@ -66,47 +82,49 @@
   </div>
 </template>
 <script>
-import { backend, setAuthToken } from 'src/marketplace/backend'
-import { formatRole } from 'src/marketplace/utils'
-import { useMarketplaceStore } from 'src/stores/marketplace'
-import { useQuasar } from 'quasar'
-import { i18n } from 'src/boot/i18n'
-import { computed, defineComponent, ref } from 'vue'
-import blankUserImg from 'src/assets/blank_user_image.webp'
+import { backend, setAuthToken } from "src/marketplace/backend";
+import { formatRole } from "src/marketplace/utils";
+import { useMarketplaceStore } from "src/stores/marketplace";
+import { useQuasar } from "quasar";
+import { i18n } from "src/boot/i18n";
+import { computed, defineComponent, ref } from "vue";
+import blankUserImg from "src/assets/blank_user_image.webp";
 
-const { t } = i18n.global
+const { t } = i18n.global;
 
 export default defineComponent({
-  name: 'MarketplaceHeader',
+  name: "MarketplaceHeader",
   props: {
-    title: { type: String, default: t('Marketplace') },
+    title: { type: String, default: t("Marketplace") },
   },
   setup() {
-    const $q = useQuasar()
-    const marketplaceStore = useMarketplaceStore()
-    const openUserMenu = ref(false)
+    const $q = useQuasar();
+    const marketplaceStore = useMarketplaceStore();
+    const openUserMenu = ref(false);
 
     const userImage = computed(() => {
-      if (marketplaceStore.user.profilePictureUrl) return marketplaceStore.user.profilePictureUrl
+      if (marketplaceStore.user.profilePictureUrl)
+        return marketplaceStore.user.profilePictureUrl;
 
       if (marketplaceStore.user.firstName || marketplaceStore.user.lastName) {
-        const fullName = `${marketplaceStore.user.firstName} ${marketplaceStore.user.lastName}`
-        return `https://api.dicebear.com/5.x/initials/svg?seed=${fullName}`
+        const fullName = `${marketplaceStore.user.firstName} ${marketplaceStore.user.lastName}`;
+        return `https://api.dicebear.com/5.x/initials/svg?seed=${fullName}`;
       }
-      return blankUserImg
-    })
+      return blankUserImg;
+    });
 
     async function logOut() {
-      try{
-        $q.loading.show({ group: 'logout' })
-        const customConfig = { forceHeartbeat: true }
-        await backend.post(`users/revoke_token/`, undefined, { customConfig })
+      try {
+        $q.loading.show({ group: "logout" });
+        const customConfig = { forceHeartbeat: true };
+        await backend
+          .post(`users/revoke_token/`, undefined, { customConfig })
           .catch(console.error)
-          .then(() => $q.loading.hide('logout'))
-        setAuthToken(undefined)
-        marketplaceStore.setUser(null)
+          .then(() => $q.loading.hide("logout"));
+        setAuthToken(undefined);
+        marketplaceStore.setUser(null);
       } finally {
-        $q.loading.hide('logout')
+        $q.loading.hide("logout");
       }
     }
 
@@ -118,14 +136,14 @@ export default defineComponent({
 
       // utils funcs
       formatRole,
-    }
+    };
   },
-})
+});
 </script>
 <style scoped lang="scss">
 .user-icon {
   height: 45px;
   width: 45px;
-  overflow:hidden;
+  overflow: hidden;
 }
 </style>
--- src/pages/marketplace/auth/LoginPage.vue
+++ src/pages/marketplace/auth/LoginPage.vue
@@ -1,23 +1,27 @@
 <template>
-  <q-page class="q-pa-md" :style="$q.platform.is.ios ? 'padding-top: 35px;' : ''">
+  <q-page class="q-pa-md">
     <div class="row items-center q-mt-lg q-mb-md">
       <div class="q-space">
-        <div class="text-h4">{{ $t('Marketplace') }}</div>
+        <div class="text-h4">{{ $t("Marketplace") }}</div>
       </div>
     </div>
     <q-card>
       <q-card-section>
-        <div class="text-h5">{{ $t('SignIn') }}</div>
+        <div class="text-h5">{{ $t("SignIn") }}</div>
         <div class="q-pb-md q-pt-sm">
-          <q-separator/>
+          <q-separator />
         </div>
         <div v-if="errors.detail?.length > 0" class="q-py-sm">
           <div class="bg-red q-px-sm q-py-md rounded-borders">
-            <div v-for="(error, index) in errors?.detail" :key="index" style="word-break:break-word;">
+            <div
+              v-for="(error, index) in errors?.detail"
+              :key="index"
+              style="word-break: break-word"
+            >
               {{ capitalize(error) }}
             </div>
           </div>
-        </div>  
+        </div>
         <q-form @submit="() => login()" class="q-gutter-y-md">
           <q-input
             dense
@@ -68,103 +72,119 @@
   </q-page>
 </template>
 <script>
-import { useQuasar } from 'quasar'
-import { backend, setAuthToken } from 'src/marketplace/backend'
-import { errorParser } from 'src/marketplace/utils'
-import { useMarketplaceStore } from 'src/stores/marketplace'
-import { capitalize, defineComponent, ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { useI18n } from 'vue-i18n'
+import { useQuasar } from "quasar";
+import { backend, setAuthToken } from "src/marketplace/backend";
+import { errorParser } from "src/marketplace/utils";
+import { useMarketplaceStore } from "src/stores/marketplace";
+import { capitalize, defineComponent, ref } from "vue";
+import { useRouter } from "vue-router";
+import { useI18n } from "vue-i18n";
 
 export default defineComponent({
-  name: 'LoginPage',
+  name: "LoginPage",
   props: {
     redirectTo: String,
     username: String,
   },
   setup(props) {
-    const { t } = useI18n()
-    const $q = useQuasar()
-    const $router = useRouter()
-    const marketplaceStore = useMarketplaceStore()
-    const loading = ref(false)
+    const { t } = useI18n();
+    const $q = useQuasar();
+    const $router = useRouter();
+    const marketplaceStore = useMarketplaceStore();
+    const loading = ref(false);
     const formData = ref({
-      username: (props.username ?? marketplaceStore.lastLoggedInUsername) || '',
-      password: '',
-    })
+      username: (props.username ?? marketplaceStore.lastLoggedInUsername) || "",
+      password: "",
+    });
     const errors = ref({
       detail: [],
-      username: '',
-      password: '',
-    })
+      username: "",
+      password: "",
+    });
     function clearErrors() {
-      errors.value.detail = []
-      errors.value.username = ''
-      errors.value.password = ''
+      errors.value.detail = [];
+      errors.value.username = "";
+      errors.value.password = "";
     }
 
     function login() {
       const data = {
         username: formData.value.username,
         password: formData.value.password,
-      }
+      };
 
-      clearErrors()
-      loading.value = true
-      backend.post('users/get_auth_token/', data)
+      clearErrors();
+      loading.value = true;
+      backend
+        .post("users/get_auth_token/", data)
         .then(async (response) => {
           if (response?.data?.auth_token) {
-            console.log('Set auth token:', await setAuthToken(response?.data?.auth_token))
+            console.log(
+              "Set auth token:",
+              await setAuthToken(response?.data?.auth_token)
+            );
           }
           if (response?.data?.user_id) {
-            marketplaceStore.setUser({ id: response?.data?.user_id })
-            marketplaceStore.refetchShop()
-            marketplaceStore.refreshUser({ silent: false })
-              .then(response => {
-                marketplaceStore.lastLoggedInUsername = response?.data?.username || ''
-                return response
+            marketplaceStore.setUser({ id: response?.data?.user_id });
+            marketplaceStore.refetchShop();
+            marketplaceStore
+              .refreshUser({ silent: false })
+              .then((response) => {
+                marketplaceStore.lastLoggedInUsername =
+                  response?.data?.username || "";
+                return response;
               })
-              .catch(error => {
+              .catch((error) => {
                 $q.notify({
-                  message: t('FailedToFetchUserData'),
-                  type: 'negative',
-                })
-                return Promise.reject(error)
+                  message: t("FailedToFetchUserData"),
+                  type: "negative",
+                });
+                return Promise.reject(error);
               })
               .then(() => {
-                if (props.redirectTo) $router.replace(props.redirectTo)
-                else $router.replace({ name: 'marketplace' })
-              })
+                if (props.redirectTo) $router.replace(props.redirectTo);
+                else $router.replace({ name: "marketplace" });
+              });
           }
-          return response
+          return response;
         })
-        .catch(error => {
-          console.error(error)
-          const username = error?.response?.data?.username
-          const password = error?.response?.data?.password
-          const detail = error?.response?.data?.non_field_errors || error?.response?.data?.detail
-          if (username) errors.value.username = Array.isArray(username) ? username.join(', ') : username
-          if (password) errors.value.password = Array.isArray(password) ? password.join(', ') : password
-          if (detail) errors.value.detail = Array.isArray(detail) ? detail : [detail]
+        .catch((error) => {
+          console.error(error);
+          const username = error?.response?.data?.username;
+          const password = error?.response?.data?.password;
+          const detail =
+            error?.response?.data?.non_field_errors ||
+            error?.response?.data?.detail;
+          if (username)
+            errors.value.username = Array.isArray(username)
+              ? username.join(", ")
+              : username;
+          if (password)
+            errors.value.password = Array.isArray(password)
+              ? password.join(", ")
+              : password;
+          if (detail)
+            errors.value.detail = Array.isArray(detail) ? detail : [detail];
 
-          console.log(error?.response)
-          const isThrottled = error?.response?.status === 429
-          const retryAfter = error?.response?.headers?.['Retry-After']
+          console.log(error?.response);
+          const isThrottled = error?.response?.status === 429;
+          const retryAfter = error?.response?.headers?.["Retry-After"];
           if (isThrottled && !errors.value.detail?.length) {
-            let msg = t('FrequentActionErrMsg1')
-            if (retryAfter) msg += t(
-              'FrequentActionErrMsg2',
-              { seconds: retryAfter },
-              `. Try again after ${retryAfter} seconds`
-            )
-            else msg += t('FrequentActionErrMsg3')
+            let msg = t("FrequentActionErrMsg1");
+            if (retryAfter)
+              msg += t(
+                "FrequentActionErrMsg2",
+                { seconds: retryAfter },
+                `. Try again after ${retryAfter} seconds`
+              );
+            else msg += t("FrequentActionErrMsg3");
 
-            errors.value.detail = [msg]
+            errors.value.detail = [msg];
           }
         })
         .finally(() => {
-          loading.value = false
-        })
+          loading.value = false;
+        });
     }
 
     return {
@@ -175,7 +195,7 @@ export default defineComponent({
 
       // utils funcs
       capitalize,
-    }
+    };
   },
-})
+});
 </script>
